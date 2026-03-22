c1db53ccf7f1ce351891e21dc000257cecae697d
Priscilla Romero <146582575+prosoli@users.noreply.github.com>
2026-03-21 21:24:39 -0600

    fix: Reorganizacion de docs dentro de server

---
+++ Proyecto/server/utils/Auxiliares.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <iostream>
+#include "../../models/operacionesstructs.h"
+#include "../../models/elementolista.h"
+
+PtrElemento buscarElemento(PtrElemento& lista, int indice);
+
+//Operaciones del CRUD
+
+//Crear (C)
+void insertarElemento(PtrElemento& lista, OperacionesStructs operaciones);
+
+//Leer (R)
+void leerElementos(PtrElemento& lista);
+
+//Actualizar (U)
+void actualizarElemento(PtrElemento& lista);
+
+//Eliminar (D)
+void eliminarElemento(PtrElemento& lista);
+//
+
+//Función para eliminar una lista por completo
+void eliminarLista(PtrElemento& lista);
