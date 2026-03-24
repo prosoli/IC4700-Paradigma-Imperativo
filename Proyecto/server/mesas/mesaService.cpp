f36b7d11966fc467af851902f64b71ad5b3df2ad
Daniel Sere <87248193+DanielSere@users.noreply.github.com>
2026-03-23 23:56:36 -0600

    add: definir funciones de mesa

--- Proyecto/server/mesas/mesaService.cpp
+++ Proyecto/server/mesas/mesaService.cpp
@@ -1,1 +1,13 @@
+#include "mesaService.h"
 
+static int numeroMesas = 10;  // valor por defecto
+
+int obtenerNumeroMesas() {
+    return numeroMesas;
+}
+
+void actualizarNumeroMesas(int nuevoNumero) {
+    if (nuevoNumero >= 0) {
+        numeroMesas = nuevoNumero;
+    }
+}
