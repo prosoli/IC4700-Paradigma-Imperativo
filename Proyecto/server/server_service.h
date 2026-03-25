5496274b03a3ea0f4fcffa098d56b80d287407a3
Priscilla Romero <146582575+prosoli@users.noreply.github.com>
2026-03-25 09:06:31 -0600

    fix: Declaracion de handlers

--- Proyecto/server/server_service.h
+++ Proyecto/server/server_service.h
@@ -9,9 +9,12 @@
 bool parseCreateOrder(const nlohmann::json& json_msg, int& mesa, std::vector<std::pair<std::string, int>>& productos, std::string& error);
 
 nlohmann::json createOrderHandler(const nlohmann::json& json_msg);
+nlohmann::json viewTablesHandler(const nlohmann::json& json_msg);
 
 nlohmann::json productoAJson(void* informacion);
 
 nlohmann::json generarListaProductos(PtrElemento lista);
 
 std::string procesarMensajeServidor(const std::string& mensaje);
+
+nlohmann::json viewOrdersHandler(const nlohmann::json& json_msg);
